2028f4312b391e8b368ab8412489fb7846d82e13
Deepesh <dsoni.ds21@gmail.com>
2023-09-11 11:14:07 +0530

    fix styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { trackEvent } from "@/utils/helpers";
 import { useEffect } from "react";
 import Listing from "../components/saloonListing";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   useEffect(() => {
