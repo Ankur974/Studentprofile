64f41b130453dd6e5d5a2f7e5b0b92a7ab895365
Deepesh <dsoni.ds21@gmail.com>
2023-09-12 10:08:43 +0530

    refactor filters

--- src/components/saloonListing/Filter.jsx
+++ src/components/saloonListing/Filter.jsx
@@ -1,12 +1,72 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import FlexBox from "../common/ui/FlexBox";
-import { H2 } from "../common/ui/Headings";
-import { Body2 } from "../common/ui/Headings";
+import { H2, Body2 } from "../common/ui/Headings";
 import { ACCENT_500, ACCENT_800, PRIMARY_800 } from "../common/ui/colors";
 import Rating from "../common/ratings/Ratings";
 import { RxCheck, RxCross1 } from "react-icons/rx";
 import Chip from "../common/ui/Chips";
+
+const services = [
+  {
+    id: 1,
+    name: "Haircut Service",
+  },
+  {
+    id: 2,
+    name: "Nails",
+  },
+  {
+    id: 3,
+    name: "Makeup",
+  },
+  {
+    id: 4,
+    name: "Styling",
+  },
+];
+const availability = [
+  {
+    id: 1,
+    name: "Today",
+  },
+  {
+    id: 2,
+    name: "Tomorrow",
+  },
+  {
+    id: 3,
+    name: "Pick a date",
+  },
+];
+const type = [
+  {
+    id: 1,
+    name: "Salon",
+  },
+  {
+    id: 2,
+    name: "Stylist",
+  },
+  {
+    id: 3,
+    name: "Other",
+  },
+];
+const salonPref = [
+  {
+    id: 1,
+    name: "Male",
+  },
+  {
+    id: 2,
+    name: "Female",
+  },
+  {
+    id: 3,
+    name: "Unisex",
+  },
+];
 
 const Wrapper = styled(FlexBox)`
   width: 100%;
@@ -23,110 +83,54 @@ const Img = styled.img`
   width: 1rem;
 `;
 
+const NewBox = styled(FlexBox)`
+  border-bottom: 1px solid #d7d7d7;
+`;
+
+const FilterChip = ({ name, selected, onClick }) => (
+  <Chip width="fit-content" selected={selected} onClick={onClick}>
+    <FlexBox align="center" columnGap="0.2rem">
+      {selected && <RxCheck color={PRIMARY_800} />}
+      <Body2 bold color={selected ? PRIMARY_800 : ACCENT_800}>
+        {name}
+      </Body2>
+    </FlexBox>
+  </Chip>
+);
+
 const Filter = () => {
-  const [selectedServices, setSelectedServices] = useState([]);
-  const [selectedAvailability, setSelectedAvailability] = useState([]);
-  const [selectedtype, setSelectedType] = useState([]);
-  const [selectedPref, setSelectedPref] = useState([]);
-  const services = [
-    {
-      id: 1,
-      name: "Haircut Service",
-    },
-    {
-      id: 2,
-      name: "Nails",
-    },
-    {
-      id: 3,
-      name: "Makeup",
-    },
-    {
-      id: 4,
-      name: "Styling",
-    },
-  ];
-  const availability = [
-    {
-      id: 1,
-      name: "Today",
-    },
-    {
-      id: 2,
-      name: "Tomorrow",
-    },
-    {
-      id: 3,
-      name: "Pick a date",
-    },
-  ];
-  const type = [
-    {
-      id: 1,
-      name: "Salon",
-    },
-    {
-      id: 2,
-      name: "Stylist",
-    },
-    {
-      id: 3,
-      name: "Other",
-    },
-  ];
-  const salonPref = [
-    {
-      id: 1,
-      name: "Male",
-    },
-    {
-      id: 2,
-      name: "Female",
-    },
-    {
-      id: 3,
-      name: "Unisex",
-    },
-  ];
-  const handleClickServices = name => {
-    const services = new Set(selectedServices);
-    if (services.has(name)) {
-      services.delete(name);
-    } else {
-      services.add(name);
-    }
-    setSelectedServices([...services]);
+  const [selectedFilters, setSelectedFilters] = useState({
+    services: [],
+    availability: [],
+    type: [],
+    pref: [],
+  });
+
+  const toggleFilter = (filterType, name) => {
+    setSelectedFilters(prevFilters => {
+      const updatedFilters = { ...prevFilters };
+      const selected = updatedFilters[filterType].includes(name);
+      if (selected) {
+        updatedFilters[filterType] = updatedFilters[filterType].filter(
+          item => item !== name
+        );
+      } else {
+        updatedFilters[filterType] = [...updatedFilters[filterType], name];
+      }
+      return updatedFilters;
+    });
   };
-  const handleClickAvailablity = name => {
-    const services = new Set(selectedAvailability);
-    if (services.has(name)) {
-      services.delete(name);
-    } else {
-      services.add(name);
-    }
-    setSelectedAvailability([...services]);
-  };
-  const handleClickPref = name => {
-    const services = new Set(selectedPref);
-    if (services.has(name)) {
-      services.delete(name);
-    } else {
-      services.add(name);
-    }
-    setSelectedPref([...services]);
-  };
-  const handleClicktype = name => {
-    const services = new Set(selectedtype);
-    if (services.has(name)) {
-      services.delete(name);
-    } else {
-      services.add(name);
-    }
-    setSelectedType([...services]);
-  };
-  const NewBox = styled(FlexBox)`
-    border-bottom: 1px solid #d7d7d7;
-  `;
+
+  const renderFilterChips = (filterType, data) =>
+    data.map(item => (
+      <FilterChip
+        key={item.id}
+        name={item.name}
+        selected={selectedFilters[filterType].includes(item.name)}
+        onClick={() => toggleFilter(filterType, item.name)}
+      />
+    ));
+
   return (
     <Wrapper column>
       <NewBox justify="space-between">
@@ -141,108 +145,17 @@ const Filter = () => {
       <Rating />
       <H2 bold>Services</H2>
       <FlexBox columnGap="0.3rem">
-        {services.map(item => (
-          <Chip
-            key={item.id}
-            width="fit-content"
-            selected={selectedServices.includes(item.name)}
-            onClick={() => handleClickServices(item.name)}
-          >
-            <FlexBox align="center" columnGap="0.2rem">
-              {selectedServices.includes(item.name) && (
-                <RxCheck color={PRIMARY_800} />
-              )}
-              <Body2
-                bold
-                color={
-                  selectedServices.includes(item.name)
-                    ? PRIMARY_800
-                    : ACCENT_800
-                }
-              >
-                {item.name}
-              </Body2>
-            </FlexBox>
-          </Chip>
-        ))}
+        {renderFilterChips("services", services)}
       </FlexBox>
       <H2 bold>Availability</H2>
       <FlexBox columnGap="0.3rem">
-        {availability.map(item => (
-          <Chip
-            key={item.id}
-            width="fit-content"
-            selected={selectedAvailability.includes(item.name)}
-            onClick={() => handleClickAvailablity(item.name)}
-          >
-            <FlexBox align="center" columnGap="0.2rem">
-              {selectedAvailability.includes(item.name) && (
-                <RxCheck color={PRIMARY_800} />
-              )}
-              <Body2
-                bold
-                color={
-                  selectedAvailability.includes(item.name)
-                    ? PRIMARY_800
-                    : ACCENT_800
-                }
-              >
-                {item.name}
-              </Body2>
-            </FlexBox>
-          </Chip>
-        ))}
+        {renderFilterChips("availability", availability)}
       </FlexBox>
       <H2 bold>Type</H2>
-      <FlexBox columnGap="0.3rem">
-        {salonPref.map(item => (
-          <Chip
-            key={item.id}
-            width="fit-content"
-            selected={selectedtype.includes(item.name)}
-            onClick={() => handleClicktype(item.name)}
-          >
-            <FlexBox align="center" columnGap="0.2rem">
-              {selectedtype.includes(item.name) && (
-                <RxCheck color={PRIMARY_800} />
-              )}
-              <Body2
-                bold
-                color={
-                  selectedtype.includes(item.name) ? PRIMARY_800 : ACCENT_800
-                }
-              >
-                {item.name}
-              </Body2>
-            </FlexBox>
-          </Chip>
-        ))}
-      </FlexBox>
-
+      <FlexBox columnGap="0.3rem">{renderFilterChips("type", type)}</FlexBox>
       <H2 bold>Salon Preference</H2>
       <FlexBox columnGap="0.3rem">
-        {salonPref.map(item => (
-          <Chip
-            key={item.id}
-            width="fit-content"
-            selected={selectedPref.includes(item.name)}
-            onClick={() => handleClickPref(item.name)}
-          >
-            <FlexBox align="center" columnGap="0.2rem">
-              {selectedPref.includes(item.name) && (
-                <RxCheck color={PRIMARY_800} />
-              )}
-              <Body2
-                bold
-                color={
-                  selectedPref.includes(item.name) ? PRIMARY_800 : ACCENT_800
-                }
-              >
-                {item.name}
-              </Body2>
-            </FlexBox>
-          </Chip>
-        ))}
+        {renderFilterChips("pref", salonPref)}
       </FlexBox>
     </Wrapper>
   );
