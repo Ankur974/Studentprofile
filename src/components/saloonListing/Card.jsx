85f05195b78af20b1e056cc0960b540f15df49cb
Ayushkumar28 <ayushshrivastava346@gmail.com>
2023-09-15 15:47:40 +0530

    card ui fixes final

--- src/components/saloonListing/Card.jsx
+++ src/components/saloonListing/Card.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import FlexBox from "../common/ui/FlexBox";
 import styled from "styled-components";
-import { H2, H4 } from "../common/ui/Headings";
+import { ButtonText, H2, H4 } from "../common/ui/Headings";
 import { Body2 } from "../common/ui/Headings";
 import { BsGenderMale } from "react-icons/bs";
 import { IoLocationOutline } from "react-icons/io5";
-import { AiOutlineHeart } from "react-icons/ai";
+import { SlHeart } from "react-icons/sl";
 import {
   ACCENT_0,
   ACCENT_700,
+  ACCENT_800,
   PRIMARY_800,
   RATEBACKGROUND,
+  SECONDARY_900,
   STARCOLOR,
   listingChip,
 } from "../common/ui/colors";
-import Button from "../common/ui/Buttons";
 import Chip from "../common/ui/Chips";
 import { AiFillStar } from "react-icons/ai";
 import { CiDiscount1 } from "react-icons/ci";
+import { Button } from "../common/ui/Buttons";
 
 const Wrapper = styled(FlexBox)`
   border: 1px solid ${listingChip};
   padding: 1rem;
-  width:100%;
-  border-radius:10px;
+  width: 100%;
+  border-radius: 10px;
   max-width: 23.75rem;
   height: 100%;
-  row-gap: 0.5rem;
+  row-gap: 0.8rem;
   margin: auto;
 `;
 
@@ -58,7 +60,7 @@ const Banner = styled(FlexBox)`
 const Img = styled.img`
   height: 13.4rem;
   object-fit: cover;
-  border-radius:10px;
+  border-radius: 0.625rem;
 `;
 
 const BannerContent = styled(FlexBox)`
@@ -70,8 +72,12 @@ const BannerContent = styled(FlexBox)`
 
 const BannerContent2 = styled(FlexBox)`
   position: absolute;
-  bottom: 0;   
+  bottom: -0.9rem;
+  height: 2rem;
+  width: ;
 `;
+
+const Img2 = styled(FlexBox);
 
 const Card = () => {
   return (
@@ -79,8 +85,14 @@ const Card = () => {
       <Banner column>
         <Img src="/assets/banner-new.svg" alt="Card1" />
         <BannerContent columnGap="13rem" align="center">
-          <FlexBox borderRadius="0.1875rem" width="4rem" align="center" justify="center" backgroundColor="#FFFFFF80">
-            <H4 color="white" >Popular</H4>
+          <FlexBox
+            borderRadius="0.1875rem"
+            width="4rem"
+            align="center"
+            justify="center"
+            backgroundColor="#FFFFFF80"
+          >
+            <H4 color="white">Popular</H4>
           </FlexBox>
           <FlexBox
             align="center"
@@ -90,27 +102,31 @@ const Card = () => {
             width="3.5rem"
             columnGap="0.5rem"
           >
-            <AiFillStar color={STARCOLOR} />
+            {/* <AiFillStar color={STARCOLOR} /> */}
+            <img src="/assets/star.svg" />
             <Body2 color={ACCENT_0}>4.2</Body2>
           </FlexBox>
         </BannerContent>
-        <BannerContent2
-          align="center"
-          width="100%"
-          columnGap="0.5rem"
-          backgroundColor={PRIMARY_800}
-          borderRadius="0rem 0rem 0.625rem 0.625rem"
-          padding="0 0 0 1rem"
-        >
-          <CiDiscount1 />
-          <Body2>15% of on first visit</Body2>
-        </BannerContent2>
+        <FlexBox justify="center">
+          <BannerContent2
+            align="center"
+            width="85%"
+            columnGap="0.5rem"
+            backgroundColor={PRIMARY_800}
+            borderRadius="0.3rem"
+            padding="0 0 0 1rem"
+            justify="center"
+          >
+            <CiDiscount1 />
+            <Body2>15% of on first visit</Body2>
+          </BannerContent2>
+        </FlexBox>
       </Banner>
 
       <FlexBox column rowGap="0.38rem">
-        <FlexBox justify="space-between" >
+        <FlexBox justify="space-between" padding="10px 0 0 0">
           <H2>Gigi's Salon</H2>
-          <AiOutlineHeart size="1.25rem" />
+          <SlHeart size="1.5rem" color={PRIMARY_800} fill="black" />
         </FlexBox>
 
         <FlexBox columnGap="0.75rem">
@@ -134,9 +150,7 @@ const Card = () => {
           </Chip>
         ))}
       </FlexBox>
-      {/* <Button>
-        <Body2>Details</Body2>
-      </Button> */}
+      <Button primary>View Details</Button>
     </Wrapper>
   );
 };
