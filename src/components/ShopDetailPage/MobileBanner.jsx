dfe04a593f1fa5a6e56302a2f432bcf67dbb7c52
Deepesh <dsoni.ds21@gmail.com>
2023-11-02 18:56:56 +0530

    update salon details page

--- src/components/ShopDetailPage/MobileBanner.jsx
+++ src/components/ShopDetailPage/MobileBanner.jsx
@@ -1,48 +1,26 @@
 import React from "react";
-import { device } from "../common/ui/Resposive";
 import FlexBox from "../common/ui/FlexBox";
 import styled from "styled-components";
 import SalonInfo from "./SalonInfo";
-import { FaRegShareSquare, FaRegHeart } from "react-icons/fa";
-import { ACCENT_0, ACCENT_800 } from "../common/ui/colors";
 
 const Banner = styled(FlexBox)`
   width: 100%;
   flex-direction: column;
-  column-gap: 20px;
-  // padding: 1rem;
-  row-gap: 20px;
-  @media ${device.laptop} {
-    display: none;
-  }
+  gap: 1rem;
 `;
+
 const Img = styled.img`
   height: 100%;
   width: 100%;
-  opacity: 1;
 `;
-const BannerContainer = styled(FlexBox)`
-  width: 100%;
-  position: relative;
-`;
-const Icons = styled(FlexBox)`
-  position: absolute;
-  top: 2%;
-  right: 2%;
-  column-gap: 10px;
-`;
+
 const MobileBanner = () => {
   return (
     <Banner>
-      <BannerContainer>
-        <Img src="/assets/banner-new.svg" />
-        <Icons>
-          <FaRegShareSquare color={ACCENT_800} size="25px" />
-          <FaRegHeart color={ACCENT_800} size="25px" />
-        </Icons>
-      </BannerContainer>
+      <Img src="/assets/banner-new.svg" />
       <SalonInfo />
     </Banner>
   );
 };
+
 export default MobileBanner;
--- src/components/ShopDetailPage/index.jsx
+++ src/components/ShopDetailPage/index.jsx
@@ -4,15 +4,34 @@ import styled from "styled-components";
 import FlexBox from "../common/ui/FlexBox";
 import Tabs from "../common/ui/Tabs";
 import Services from "./Services";
-import HeroBanner from "./Banner";
 import RealAbout from "./About";
+import MobileBanner from "./MobileBanner";
+import DesktopBanner from "./DesktopBanner";
+import { device } from "../common/ui/Resposive";
 
 const Container = styled(FlexBox)`
   flex-direction: column;
   width: 100%;
   justify-content: center;
-  row-gap: 2rem;
+  row-gap: 1.5rem;
   margin: auto;
+`;
+
+const HideMobile = styled.div`
+  display: none;
+  @media ${device.laptop} {
+    display: block;
+    width: 86.67%;
+    max-width: 75rem;
+    margin: auto;
+  }
+`;
+
+const HideDesktop = styled(FlexBox)`
+  width: 100%;
+  @media ${device.laptop} {
+    display: none;
+  }
 `;
 
 const Wrapper = styled(FlexBox)`
@@ -28,7 +47,12 @@ const Tab = styled(FlexBox)``;
 const ShopDetailPage = () => {
   return (
     <Container>
-      <HeroBanner />
+      <HideMobile>
+        <DesktopBanner />
+      </HideMobile>
+      <HideDesktop>
+        <MobileBanner />
+      </HideDesktop>
       <Wrapper>
         <Tabs>
           <Tab title="Services">
