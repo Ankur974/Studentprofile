5e81b0389967734e26436bfe3e5971fc20b0ae22
Deepesh <dsoni.ds21@gmail.com>
2023-11-03 15:09:43 +0530

    update carousel style

--- src/components/common/Carousel.jsx
+++ src/components/common/Carousel.jsx
@@ -5,16 +5,28 @@ import { ACCENT_0, ACCENT_800 } from "./ui/colors";
 import { FaRegShareSquare } from "react-icons/fa";
 import LoveReact from "./ui/LoveReact";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import "swiper/css";
+import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import { Navigation } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 
 const Carousel = ({ images }) => {
   const [clicked, setClicked] = useState(false);
+
+  const Wrapper = styled(FlexBox)`
+    width: 100vw;
+    height: 100vh;
+    background-color: ${ACCENT_800};
+    overflow: hidden;
+    padding: 2.5rem;
+  `;
+
   const Container = styled(FlexBox)`
-    width: 100%;
-    padding: 1rem;
+    width: 90%;
+    max-width: 50rem;
+    margin: auto;
   `;
 
   const Img = styled.img`
@@ -22,32 +34,31 @@ const Carousel = ({ images }) => {
     height: 100%;
   `;
 
-  const Wrapper = styled(FlexBox)`
+  const ScrollableList = styled(FlexBox)`
     width: 100%;
-    height: 60.725rem;
-    padding: 1rem;
-    background-color: ${ACCENT_800};
-    position: relative;
+    height: 100%;
     justify-content: center;
     align-items: center;
-  `;
 
-  const ScrollableList = styled(FlexBox)`
-    width: 50%;
-    justify-content: space-between;
-    align-items;center;
-    height: 65%;
+    .swiper-button-prev {
+      background-image: url("images/arrow-left.png"); //todo - update icon - SR
+      background-size: contain;
+      background-position: 50% 60%;
+      background-repeat: no-repeat;
+      left: 69px;
+    }
+
+    .swiper-pagination-fraction {
+    }
   `;
 
   const Icon = styled(FlexBox)`
-    position: absolute;
-    top: 1%;
-    right: 2%;
-    column-gap: 20px;
+    gap: 1rem;
+    justify-content: flex-end;
   `;
 
   return (
-    <Wrapper>
+    <Wrapper column>
       <Icon>
         <FaRegShareSquare color={ACCENT_0} />
         <LoveReact clicked={clicked} setclicked={setClicked} color={ACCENT_0} />
@@ -55,9 +66,12 @@ const Carousel = ({ images }) => {
 
       <ScrollableList>
         <Swiper
-          modules={[Navigation]}
+          modules={[Pagination, Navigation]}
           spaceBetween={8}
           slidesPerView={1}
+          pagination={{
+            type: "fraction",
+          }}
           navigation
         >
           {images?.map(data => (
