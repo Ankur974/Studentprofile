98836711a1190febc8f083659308d259a5daea0b
sujitrungta121 <75289960+sujitrungta121@users.noreply.github.com>
2023-11-17 11:39:13 +0530

    fix:image issue solved in homepage card (#26)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,13 +3,13 @@ import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import { Body2 } from "@common/ui/Headings";
 import styled from "styled-components";
 import FlexBox from "@common/ui/FlexBox";
 import { device } from "@common/ui/Resposive";
 import Card from "./Card";
 import OfferCard2 from "./OfferCard2";
-
-
+import { ACCENT_0 } from "../common/ui/colors";
 
 const Wrapper = styled(FlexBox)`
   flex-direction: column;
@@ -27,12 +27,25 @@ const Wrapper = styled(FlexBox)`
 
 const ScrollableList = styled.div`
   width: 100%;
+  position: relative;
 `;
 
 const CardWrapper = styled.div`
   width: 24rem;
 `;
-
+const AddImage = styled.div`
+  background-color: black;
+  position: absolute;
+  bottom: 5%;
+  right: 2%;
+  z-index: 5;
+  padding: 0.5rem;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
+const handleClick = () => {
+  alert("you have clicked");
+};
 const offerCard2data = [
   {
     id: 1,
@@ -40,8 +53,8 @@ const offerCard2data = [
       "linear-gradient(320deg, rgba(209,0,0,0.6090805951286764) 0%, rgba(253,45,57,0.9508172898065477) 100%);",
     title: "GET 40% OFF",
     desc: "Lorem ipsum, dolor sit amet consectetur.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 2,
@@ -49,8 +62,8 @@ const offerCard2data = [
       "linear-gradient(120deg, rgba(255,0,0,0.6) 0%, rgba(255,102,102,0.9) 100%);",
     title: "20% OFF Haircut",
     desc: "Revamp your look with a 20% discount on haircuts.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 3,
@@ -58,8 +71,8 @@ const offerCard2data = [
       "linear-gradient(45deg, rgba(0,102,204,0.6) 0%, rgba(51,153,255,0.9) 100%);",
     title: "Manicure & Pedicure Combo",
     desc: "Treat yourself to a relaxing manicure and pedicure combo at a special price.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 4,
@@ -67,8 +80,8 @@ const offerCard2data = [
       "linear-gradient(160deg, rgba(255,153,0,0.6) 0%, rgba(255,204,102,0.9) 100%);",
     title: "Massage Therapy",
     desc: "Relieve stress with a rejuvenating massage session and save 25%.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 6,
@@ -76,8 +89,8 @@ const offerCard2data = [
       "linear-gradient(240deg, rgba(255,0,255,0.6) 0%, rgba(255,102,255,0.9) 100%);",
     title: "Nail Art Workshop",
     desc: "Learn the art of nail design in our workshop with a 10% discount.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 7,
@@ -85,8 +98,8 @@ const offerCard2data = [
       "linear-gradient(30deg, rgba(0,153,153,0.6) 0%, rgba(0,204,204,0.9) 100%);",
     title: "Hair Coloring Special",
     desc: "Transform your hair color with our special offer and save 30%.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 8,
@@ -94,8 +107,8 @@ const offerCard2data = [
       "linear-gradient(200deg, rgba(255,102,0,0.6) 0%, rgba(255,153,102,0.9) 100%);",
     title: "Aromatherapy Spa",
     desc: "Indulge in relaxation with our aromatherapy spa session and get 20% off.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 9,
@@ -103,8 +116,8 @@ const offerCard2data = [
       "linear-gradient(60deg, rgba(153,0,153,0.6) 0%, rgba(204,51,204,0.9) 100%);",
     title: "Bridal Makeup Package",
     desc: "Get ready for your special day with our bridal makeup package and save 15%.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
   {
     id: 10,
@@ -112,8 +125,8 @@ const offerCard2data = [
       "linear-gradient(260deg, rgba(102,0,204,0.6) 0%, rgba(153,51,255,0.9) 100%);",
     title: "Hair Extensions",
     desc: "Add volume and length to your hair with hair extensions at 25% off.",
-    imglogo: "/assets/coin.svg",
-    mainimg: "/assets/coin.svg",
+    imglogo: "/assets/images/coin.svg",
+    mainimg: "/assets/images/coin.svg",
   },
 ];
 
@@ -150,6 +163,9 @@ const Home = () => {
             </SwiperSlide>
           ))}
         </Swiper>
+        <AddImage onClick={handleClick}>
+          <Body2 color={ACCENT_0}>Click to add image</Body2>
+        </AddImage>
       </ScrollableList>
     </Wrapper>
   );
