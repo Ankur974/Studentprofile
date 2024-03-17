6f77447f703426a3b7362119e99a721a0aee955a
Deepesh <dsoni.ds21@gmail.com>
2024-03-17 11:42:06 +0530

    fix: fix build issues

--- src/components/HoliLandingPage/Widget.jsx
+++ src/components/HoliLandingPage/Widget.jsx
@@ -76,7 +76,7 @@ const Widget2 = () => (
       <WidgetImg src="/assets/images/holi/widget1.webp" />
     </LeftSection>
     <RightSection justify="center" rowGap="1rem">
-      <Caption bold>No hidden charges: Book with us, no fees!</Caption>
+      <Caption bold>Book with Us - Zero Fees, Zero Surprises!</Caption>
     </RightSection>
   </WidgetContainer>
 );
