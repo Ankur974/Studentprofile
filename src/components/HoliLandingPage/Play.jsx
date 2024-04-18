6c8fdce8335410a824384053833bb755bee6ea52
Deepesh Soni <dsoni.ds21@gmail.com>
2024-04-18 20:10:56 +0530

    feat: update holi landing page

--- src/components/HoliLandingPage/Play.jsx
+++ src/components/HoliLandingPage/Play.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import { WHITE } from "@common/ui/colors";
+
+import { WHITE, listingChip } from "@common/ui/colors";
 import FlexBox from "@common/ui/FlexBox";
 import styled from "styled-components";
 import { H1 } from "@common/ui/Headings";
 import { Caption } from "@common/ui/Headings";
 import { Button } from "@common/ui/Buttons";
 import { device } from "@common/ui/Responsive";
+import { categoryData } from "@metadata/CategoryData";
+import Slider from "../Home/Slider";
 
 const Wrapper = styled(FlexBox)`
   height: 100%;
@@ -28,17 +31,17 @@ const Heading = styled(H1)`
   text-align: center;
 `;
 
-const ImageContainer = styled(FlexBox)`
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-  margin-top: 2rem;
+// const ImageContainer = styled(FlexBox)`
+//   justify-content: center;
+//   align-items: center;
+//   align-self: center;
+//   margin-top: 2rem;
 
-  @media ${device.laptop} {
-    left: 50%;
-    right: 50%;
-  }
-`;
+//   @media ${device.laptop} {
+//     left: 50%;
+//     right: 50%;
+//   }
+// `;
 
 const ColorGradient = styled.span`
   background-image: linear-gradient(
@@ -51,17 +54,17 @@ const ColorGradient = styled.span`
   -webkit-text-fill-color: transparent;
 `;
 
-const Frame = styled.img`
-  width: 100%;
-  object-fit: cover;
-  height: 28rem;
-  mix-blend-mode: normal;
+// const Frame = styled.img`
+//   width: 100%;
+//   object-fit: cover;
+//   height: 28rem;
+//   mix-blend-mode: normal;
 
-  @media ${device.laptop} {
-    width: 55rem;
-    height: 22rem;
-  }
-`;
+//   @media ${device.laptop} {
+//     width: 55rem;
+//     height: 22rem;
+//   }
+// `;
 
 const SubHeading = styled(Caption)`
   font-size: 1rem;
@@ -75,6 +78,21 @@ const PlayCTA = styled(Button)`
   text-transform: none;
   font-size: 1rem;
   font-weight: 600;
+`;
+
+const Divider = styled(FlexBox)`
+  border-bottom: 1px solid ${listingChip};
+`;
+
+const SliderContainer = styled(FlexBox)`
+  margin-top: 15rem;
+  padding: 1.5rem;
+  flex-direction: column;
+  row-gap: 2rem;
+
+  @media ${device.laptop} {
+    row-gap: 4rem;
+  }
 `;
 
 const Play = ({ targetElement }) => {
@@ -94,19 +112,27 @@ const Play = ({ targetElement }) => {
         />
         <FlexBox column align="center">
           <Heading>
-            We are here to make your Holi even more{" "}
-            <ColorGradient>colorful!</ColorGradient>
+            We are here to make your salon visits even more hassle-free and{" "}
+            <ColorGradient>fabulous!</ColorGradient>
           </Heading>
           <SubHeading>
-            Enjoy a vibrant Holi with Pamprazzi! Don't worry about stains we've
-            got vouchers covered. Let's make this Holi unforgettable!
+            Kolkata, get ready! While we put the finishing touches on our
+            platform, explore our offerings—from classic haircuts to Ayurvedic
+            facials. And don’t miss the chance to spin our wheel for pre-launch
+            vouchers. It’s our way of saying,{" "}
+            <ColorGradient>“Shundor hoye jao!”</ColorGradient>
           </SubHeading>
         </FlexBox>
         <PlayCTA onClick={scrollToTarget}>Play to Win!</PlayCTA>
       </Wrapper>
-      <ImageContainer>
+      {/* <ImageContainer>
         <Frame src="/assets/images/holi/banner.webp" />
-      </ImageContainer>
+      </ImageContainer> */}
+      <SliderContainer>
+        <Divider />
+        <Slider heading="Explore our range of services" data={categoryData} />
+        <Divider />
+      </SliderContainer>
     </>
   );
 };
