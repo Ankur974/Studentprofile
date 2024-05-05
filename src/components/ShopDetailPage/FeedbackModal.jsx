0e6e377d01e73ffd5fbf9b5f238bec5018cd8f43
Ankur974 <you@example.com>
2024-05-05 12:13:29 +0530

    fix: ui

--- src/components/ShopDetailPage/FeedbackModal.jsx
+++ src/components/ShopDetailPage/FeedbackModal.jsx
@@ -4,23 +4,16 @@ import { IoCloseOutline } from "react-icons/io5";
 import { FaThumbsUp } from "react-icons/fa";
 
 import Modal from "@common/ui/Modal";
-import { Body1, H3 } from "@common/ui/Headings";
+import { Body1, H3, Body2 } from "@common/ui/Headings";
 import { Button } from "@common/ui/Buttons";
 import FlexBox from "@common/ui/FlexBox";
 import Ratings from "@common/ui/Ratings";
+import Avatar from "@common/ui/Avatar";
+import { FaCamera } from "react-icons/fa";
 
-const ModalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  backdrop-filter: blur(5px);
-  z-index: 1000;
+const Header = styled(FlexBox)`
+  justify-content: space-between;
+  padding: 1rem;
 `;
 
 const TextAreaContainer = styled.textarea`
@@ -28,21 +21,36 @@ const TextAreaContainer = styled.textarea`
   border-radius: 0.5rem;
   padding: 0.75rem;
   resize: none;
+  text-align: center;
 `;
 
-const RatingItemContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  column-gap: 1rem;
+const RatingItemContainer = styled(FlexBox)`
+  justify-content: space-between;
+`;
+
+const Body = styled(FlexBox)`
+  flex-direction: column;
+  padding: 0.5rem 1.5rem;
+`;
+const TextArea = styled(FlexBox)`
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  row-gap: 1rem;
 `;
-
+const PhotoContainer = styled(FlexBox)`
+  width: 24.9375rem;
+  padding: 0.25rem 1.5rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.75rem;
+  border: 1px solid var(--Brand-color, #533a71);
+`;
 const RatingItem = ({ title, value, onChange }) => (
   <RatingItemContainer>
-    <div>
-      <Body1>{title}</Body1>
-    </div>
-    <FlexBox align="center">
+    <Body1>{title}</Body1>
+    <FlexBox>
       <Ratings onChange={onChange} defaultValue={value} />
     </FlexBox>
   </RatingItemContainer>
@@ -89,64 +97,69 @@ const ReviewModal = ({ shopData }) => {
     <>
       <Button onClick={handleOpenModal}>Add Review</Button>
       {isOpen && (
-        <ModalContainer>
-          <Modal
-            M2
-            height="fit-content"
-            borderRadius="0.75rem"
-            togglePopup={isOpen}
-          >
-            <FlexBox justify="flex-end" padding="0.5rem">
-              <IoCloseOutline size="2rem" onClick={handleCloseModal} />
+        <Modal
+          S
+          height="fit-content"
+          borderRadius="0.75rem"
+          togglePopup={isOpen}
+        >
+          <Header>
+            <FlexBox align="center" justify="center" columnGap="0.5rem">
+              <Avatar name={shopData?.ownerDetails?.name} />
+              <FlexBox column>
+                <Body1 bold>{shopData?.ownerDetails?.name}</Body1>
+                <Body2 color="#808080">Posting with a public profile</Body2>
+              </FlexBox>
             </FlexBox>
-            <FlexBox column align="center" rowGap="1rem" padding="0.25rem" justify="space-between">
-              {!isSubmitted ? (
-                <>
-                    <H3 bold>Hey {shopData?.ownerDetails?.name}</H3>
-                    <Body1>
-                      Would you like to rate your overall experience?{" "}
-                    </Body1>
-                    <Ratings
-                      onChange={value => handleRatingChange("Overall", value)}
-                      defaultValue={ratings.Overall}
-                    />
-                  <FlexBox
-                    column
-                    columnGap="2rem"
-                    align="flex-start"
-                    justify="space-between"
-                  >
-                    {criteriaData.map((item, index) => (
-                      <RatingItem
-                        key={index}
-                        title={item.title}
-                        value={ratings[item.title]}
-                        onChange={value =>
-                          handleRatingChange(item.title, value)
-                        }
-                      />
-                    ))}
-                  </FlexBox>
+            <IoCloseOutline size="2rem" onClick={handleCloseModal} />
+          </Header>
+          <FlexBox justify="center">
+            <Ratings
+              onChange={value => handleRatingChange("Overall", value)}
+              defaultValue={ratings.Overall}
+            />
+          </FlexBox>
+          <Body>
+            {!isSubmitted ? (
+              <>
+                {criteriaData.map((item, index) => (
+                  <RatingItem
+                    key={index}
+                    title={item.title}
+                    value={ratings[item.title]}
+                    onChange={value => handleRatingChange(item.title, value)}
+                  />
+                ))}
+                <TextArea>
                   <TextAreaContainer
-                    placeholder="Write additional comment here"
+                    placeholder="Share your thoughts and experience of todays anointment."
                     value={comment}
                     onChange={handleCommentChange}
                   />
-                  <Button onClick={handleSubmit}>Submit</Button>
-                </>
-              ) : (
-                <>
-                  <FaThumbsUp size={100} />
-                  <H3 bold>Amazing</H3>
-                  <Body1>
-                    Thanks {shopData?.ownerDetails?.name} for your feedback{" "}
-                  </Body1>
-                  <Button onClick={handleCloseModal}>Go to Home</Button>
-                </>
-              )}
-            </FlexBox>
-          </Modal>
-        </ModalContainer>
+                  <PhotoContainer>
+                    <FaCamera />
+                    <Body2 color="#533A71" bold>
+                      Add some photos & videos
+                    </Body2>
+                  </PhotoContainer>
+                  <FlexBox columnGap="1rem" align="flex-start">
+                    <Button outline>Cancel</Button>
+                    <Button onClick={handleSubmit}>Post</Button>
+                  </FlexBox>
+                </TextArea>
+              </>
+            ) : (
+              <>
+                <FaThumbsUp size={100} />
+                <H3 bold>Amazing</H3>
+                <Body1>
+                  Thanks {shopData?.ownerDetails?.name} for your feedback{" "}
+                </Body1>
+                <Button onClick={handleCloseModal}>Go to Home</Button>
+              </>
+            )}
+          </Body>
+        </Modal>
       )}
     </>
   );
